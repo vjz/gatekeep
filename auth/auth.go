687647e970e02ff04fb7c5f4254d4173b9efa240
DealZen Dev <dealzen.km@gmail.com>
2025-08-03 10:00:12 -0700

    Remove hardcoded password check from auth package.

--- auth/auth.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-// Change this to a hashed check or keychain later
-const hardcodedPassword = "mySecret123"
-
-func CheckPassword(input string) bool {
-	return input == hardcodedPassword
-}
